7e8143a620eaa1b13d9a8935d3c113579f2eefa5
jnicols11 <46580604+jnicols11@users.noreply.github.com>
2020-01-24 16:37:17 -0700

    Edited RegisterContoller Added Services

--- Minesweeper2/Minesweeper2/Controllers/RegisterController.cs
+++ Minesweeper2/Minesweeper2/Controllers/RegisterController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Minesweeper2.Services.Business;
+using Minesweeper2.Services.Data;
+
 
 namespace Minesweeper.Controllers
 {
@@ -16,7 +19,9 @@ namespace Minesweeper.Controllers
 
         [HttpPost]
         public ActionResult Register()
-        { 
+        {
+            SecurityService ss = new SecurityService();
+            SecurityDAO sd = new SecurityDAO();
             return View("Home");
         }//end Register
     }//end class controller
---
+++ Minesweeper2/Minesweeper2/Services/Business/SecurityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Minesweeper2.Services.Business
+{
+    public class SecurityService
+    {
+
+    }//end Security Service
+}//end Namespace
