bfa123ac418086dbd3d06b48048cd28cfa8f4083
jnicols11 <46580604+jnicols11@users.noreply.github.com>
2020-01-27 15:47:48 -0700

    initial commit

---
+++ Minesweeper2/Minesweeper2/Minesweeper2/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Minesweeper2.Models
+{
+    public class UserModel
+    {
+        public string FirstName;
+        public string LastName;
+        public string Sex;
+        public int Age;
+        public string State;
+        public string Email;
+        public string Username;
+        public string Password;
+    }//end UserModel
+}//end namespace
---
+++ Minesweeper2/Minesweeper2/Minesweeper2/Services/Business/SecurityService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Minesweeper2.Models;
+using Minesweeper2.Services.Data;
+
+namespace Minesweeper2.Services.Business
+{
+    public class SecurityService
+    {
+        public bool Authenticate(UserModel user)
+        {
+            SecurityDAO service = new SecurityDAO();
+            return service.FindByUser(user);
+        }//end Authenticate
+    }//end Security Service
+}//end Namespace
