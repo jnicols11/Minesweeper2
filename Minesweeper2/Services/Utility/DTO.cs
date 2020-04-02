b60d87fa186ff3b89910dc69f302cc76fd345ed1
Cyrus Duncan <cyrusd4264@gmail.com>
2020-04-02 16:31:20 -0600

    Added DTO
    Added DTO Object

---
+++ Minesweeper2/Services/Utility/DTO.cs
@@ -0,0 +1,35 @@
+﻿using Minesweeper2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Minesweeper2.Services.Utility
+{
+    [DataContract]
+    public class DTO
+    {
+        public DTO(int ErrorCode, string ErrorMsg, List<StatsModel> Stats)
+        {
+            this.ErrorCode = ErrorCode;
+            this.ErrorMsg = ErrorMsg;
+            this.Stats = Stats;
+        }
+        public DTO(int ErrorCode, string ErrorMsg, List<UserModel> Users)
+        {
+            this.ErrorCode = ErrorCode;
+            this.ErrorMsg = ErrorMsg;
+            this.Users = Users;
+        }
+        [DataMember]
+        public int ErrorCode { get; set; }
+        [DataMember]
+        public string ErrorMsg { get; set; }
+        [DataMember]
+        public List<StatsModel> Stats { get; set; }
+
+        [DataMember]
+        public List<UserModel> Users { get; set; }
+    }
+}
